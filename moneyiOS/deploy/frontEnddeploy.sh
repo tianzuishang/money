61f5bffea2ffb26b83ca0b159241bdddd093dd09
王翰 <wanghan@cfets.zh>
2016-09-20 16:40:44 +0800

    #7

--- moneyiOS/deploy/frontEnddeploy.sh
+++ moneyiOS/deploy/frontEnddeploy.sh
@@ -1,17 +1,17 @@
 workspacePath="/Users/wangjam/Documents/develop/money/moneyiOS/moneyiOS.xcworkspace"
 schemeName="UATmoneyiOS"
+firToken="cbcd61de383d14ccb2494068a894a332"
+buildPath="/Users/wangjam/Documents/develop/money/moneyiOS/deploy/UATmoneyiOS"
 
-
-
+echo $workspacePath
+echo $schemeName
+echo $buildPath
 
 git checkout develop
 git pull
 
-
 xcodebuild -workspace workspacePath -scheme schemeName
+xcrun  -sdk iphoneos PackageApplication -v buildPath -o /Users/wangjam/Documents/develop/money/moneyiOS/deploy/UATmoneyiOS.ipa
 
-fir login cbcd61de383d14ccb2494068a894a332
-
-xcrun  -sdk iphoneos PackageApplication -v /Users/wangjam/Library/Developer/Xcode/DerivedData/moneyiOS-fqpazxdxfvcthkchuotjrhxlandb/Build/Products/Debug-iphoneos/UATmoneyiOS.app -o /Users/wangjam/Documents/develop/money/moneyiOS/deploy/UATmoneyiOS.ipa
-
+fir login firToken
 fir publish /Users/wangjam/Documents/develop/money/moneyiOS/deploy/UATmoneyiOS.ipa
