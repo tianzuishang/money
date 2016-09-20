acd6777e6d3d42f510b81f88d81535e699d17825
王翰 <wanghan@cfets.zh>
2016-09-20 13:47:39 +0800

    #7

---
+++ moneyBackend/updateScript/frontEnddeploy.sh
@@ -0,0 +1,11 @@
+xcodebuild -workspace moneyiOS.xcworkspace/ -scheme UATmoneyiOS
+
+
+fir login cbcd61de383d14ccb2494068a894a332
+
+
+
+xcrun  -sdk iphoneos PackageApplication -v /Users/wangjam/Library/Developer/Xcode/DerivedData/moneyiOS-fqpazxdxfvcthkchuotjrhxlandb/Build/Products/Debug-iphoneos/UATmoneyiOS.app -o /Users/wangjam/Documents/develop/money/moneyiOS/xxx.ipa
+
+
+fir publish xxx.ipa
