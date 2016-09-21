2f771b635668d7b10c96d9c3f355fd46c2a2ca8e
王翰 <wanghan@cfets.zh>
2016-09-21 11:02:59 +0800

    #7

--- moneyiOS/deploy/frontEnddeploy.sh
+++ moneyiOS/deploy/frontEnddeploy.sh
@@ -19,5 +19,4 @@ xcodebuild -workspace $workspacePath -scheme $schemeName clean
 xcodebuild -workspace $workspacePath -scheme $schemeName
 xcrun  -sdk iphoneos PackageApplication -v ${buildPath}UATmoneyiOS.app -o $ipaPath
 
-fir login $firToken
-fir publish $ipaPath
+curl --insecure -F "file=@"${$ipaPath} -F "app_id={87e6825c9b}" -F "pid={1}" -F "title={银河间}" https://api.bugly.qq.com/beta/apiv1/exp?app_key={5b8710d1-7383-4ffd-9faa-5513ad7f6422}
