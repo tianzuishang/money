97fbc5610afdf597d7ede8f2d815377c856932e3
王翰 <wanghan@cfets.zh>
2016-09-29 16:56:19 +0800

    #7

--- moneyiOS/deploy/frontEndDeploy.py
+++ moneyiOS/deploy/frontEndDeploy.py
@@ -15,35 +15,35 @@ print ("ipaPath:" + ipaPath)
 
 returnValue = os.system("git checkout develop")
 if(returnValue != 0):
-    print "git checkout develop failed:" + str(returnValue)
+    print ("git checkout develop failed:" + str(returnValue))
     exit(-1)
 
 returnValue = os.system("git pull")
 if(returnValue != 0):
-    print "git pull failed:" + str(returnValue)
+    print ("git pull failed:" + str(returnValue))
     exit(-1)
 
 returnValue = os.system("rm -f " + ipaPath)
 if(returnValue != 0):
-    print "rm " + ipaPath + " failed:" + str(returnValue)
+    print ("rm " + ipaPath + " failed:" + str(returnValue))
     exit(-1)
 
 #clean
 returnValue = os.system("xcodebuild -workspace " + workspacePath + " -scheme " + schemeName + " clean")
 if(returnValue != 0):
-    print "clean failed:" + str(returnValue)
+    print ("clean failed:" + str(returnValue))
     exit(-1)
 
 #build
 returnValue = os.system("xcodebuild -workspace " + workspacePath + " -scheme " + schemeName)
 if(returnValue != 0):
-    print "xcodebuild failed:" + str(returnValue)
+    print ("xcodebuild failed:" + str(returnValue))
     exit(-1)
 
 #xcrun
 returnValue = os.system("xcrun  -sdk iphoneos PackageApplication -v " + buildPath + " -o " + ipaPath)
 if(returnValue != 0):
-    print "xcrun failed:" + str(returnValue)
+    print ("xcrun failed:" + str(returnValue))
     exit(-1)
 
 
