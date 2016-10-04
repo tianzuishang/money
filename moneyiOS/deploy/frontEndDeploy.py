b2d38557e342ca079e604b16bdaf7e07c6466bd0
王翰 <wanghan@cfets.zh>
2016-10-04 10:27:52 +0800

    #21

--- moneyiOS/deploy/frontEndDeploy.py
+++ moneyiOS/deploy/frontEndDeploy.py
@@ -2,6 +2,8 @@
 import os
 import httplib2
 import urllib
+import re
+import json
 
 workspacePath = "/Users/wangjam/Documents/develop/money/moneyiOS/moneyiOS.xcworkspace"
 schemeName = "UATmoneyiOS"
@@ -31,10 +33,41 @@ if(returnValue != 0):
     exit(-1)
 
 #generate release note
+releaseNote = ""
+dic = {}
 returnValue = os.system("git log>gitLog.txt")
+f = open("./gitLog.txt", "r")
+for line in f.readlines():
+    words = line.split()
+    for word in words:
+        if(word[0] == '#' and len(word) > 1):
+            word = word[1:]
+            if(word.isdigit() == False):
+                print(word + ' issue is invalid')
+            else:
+                dic[word] = word
+f.close()
 
-
-
+for issue in dic.values():
+    url = "https://api.github.com/repos/cfetslab/money/issues/" + issue
+    print(url)
+    h = httplib2.Http(".cache")
+    resp, content = h.request(url, "GET")
+    #resp = json.loads(resp)
+    content = content.decode()
+    content = json.loads(content)
+    #print(content['title'])
+    if(resp.status != 200):
+        releaseNote += "#"+issue+"\n"
+    else:
+        if("pull_request" not in content.keys()):
+            if(content['state'] == 'open'):
+                releaseNote += "#"+issue+" "+content['title']+" working\n"
+            elif(content['state'] == 'closed'):
+                releaseNote += "#"+issue+" "+content['title']+" done\n"
+            else:
+                releaseNote += "#"+issue+" "+content['title']+" 状态不明\n"
+print(releaseNote)
 
 
 if(returnValue != 0):
@@ -64,7 +97,7 @@ if(returnValue != 0):
 
 #post to bugly
 
-returnValue = os.system("curl --insecure -F \"file=@"+ipaPath+"\" -F \"app_id=87e6825c9b\" -F \"pid=2\" -F \"title=money\" https://api.bugly.qq.com/beta/apiv1/exp?app_key=5b8710d1-7383-4ffd-9faa-5513ad7f6422")
+returnValue = os.system("curl --insecure -F \"file=@"+ipaPath+"\" -F \"app_id=87e6825c9b\" -F \"pid=2\" -F \"title=money\" -F \"description="+releaseNote+"\" https://api.bugly.qq.com/beta/apiv1/exp?app_key=5b8710d1-7383-4ffd-9faa-5513ad7f6422")
 if(returnValue != 0):
     print ("post to bugly failed:" + str(returnValue))
     exit(-1)
