99e338118ab2fa7974f7818f328386054f8e8f95
王翰 <wanghan@cfets.zh>
2016-10-08 15:50:35 +0800

    #26

--- moneyBackend/test/apnTest.js
+++ moneyBackend/test/apnTest.js
@@ -1,4 +1,4 @@
 var applePush = require('../apn/applePush.js')
-var token = "<8e4c5dbc 894eaf09 9735dc7f db277ca1 588c97ce 0f2c055d 10bba973 0a996de0>"
+var token = "8e4c5dbc 894eaf09 9735dc7f db277ca1 588c97ce 0f2c055d 10bba973 0a996de0"
 var msg = "测试ios10推送"
 applePush.pushMsg(token, msg)
