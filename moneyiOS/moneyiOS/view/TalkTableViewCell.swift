f8816e8ace448af553522e9d88672ba9814be0f3
王翰 <wanghan@cfets.zh>
2016-10-11 10:21:13 +0800

    #26

---
+++ moneyiOS/moneyiOS/view/TalkTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TalkTableViewCell.swift
+//  moneyiOS
+//
+//  Created by wang jam on 09/10/2016.
+//  Copyright © 2016 jam wang. All rights reserved.
+//
+
+import UIKit
+
+class TalkTableViewCell: NSObject {
+
+}
