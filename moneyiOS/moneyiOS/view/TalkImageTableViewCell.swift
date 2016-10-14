8d6caa62a6d24addf68a8d887f06673aba5da7a9
王翰 <wanghan@cfets.zh>
2016-10-14 16:50:40 +0800

    #29

---
+++ moneyiOS/moneyiOS/view/TalkImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TalkImageTableViewCell.swift
+//  moneyiOS
+//
+//  Created by wang jam on 14/10/2016.
+//  Copyright © 2016 jam wang. All rights reserved.
+//
+
+import UIKit
+
+class TalkImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ moneyiOS/moneyiOS/view/TalkMsgTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TalkMsgTableViewCell.swift
+//  moneyiOS
+//
+//  Created by wang jam on 14/10/2016.
+//  Copyright © 2016 jam wang. All rights reserved.
+//
+
+import UIKit
+
+class TalkMsgTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
