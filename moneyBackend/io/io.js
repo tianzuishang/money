722ccbc616c6164ae7a88834f6ad8bc0d55af199
王翰 <wanghan@cfets.zh>
2016-10-17 14:25:05 +0800

    #29

--- moneyBackend/io/io.js
+++ moneyBackend/io/io.js
@@ -4,48 +4,56 @@ var constant = require('../utility/constant.js');
 
 //长连接
 
-exports.connectionEntry = function(socket){
-    //长连接定义入口
-    //connection 事件触发
-    console.log('connection');
-    socket.emit('authorize', {});//请求token认证
-    socket.on('authorize', function(data){
-        //验证token
-        authe.autheToken(data.token, function(err, code){
-            if (err) {
-                log.error(err, log.getFileNameAndLineNum(__filename));
-
-                socket.emit('authorized', {
-                    code: constant.returnCode.ERROR,
-                    msg: err
-                });
-            }else{
-                if (code === constant.returnCode.TOKEN_INVALID) {
-                    log.debug('token invalid');
-                    socket.emit('authorized', {
-                        code: constant.returnCode.TOKEN_INVALID,
-                        msg: 'token invalid'
-                    });
-                }
-                if (code === constant.returnCode.TOKEN_VALID) {
-                    log.debug('token valid');
-                    socket.emit('authorized', {
-                        code: constant.returnCode.TOKEN_VALID,
-                        msg: 'token valid'
-                    });
-
-                    //定义各类事件响应
-                    ioHandle(socket);
-                }
-            }
-        });
+exports.connectionEntry = function(socket) {
+    socket.on('msg', function(data, fn){
+        console.log(data)
+        fn(data)
     });
 }
 
 
-function ioHandle(socket){
-    socket.on('data', function(data){
-        console.log('data event');
-        socket.emit('data', data);
-    });
-}
+// exports.connectionEntry = function(socket){
+//     //长连接定义入口
+//     //connection 事件触发
+//     console.log('connection');
+//     socket.emit('authorize', {});//请求token认证
+//     socket.on('authorize', function(data){
+//         //验证token
+//         authe.autheToken(data.token, function(err, code){
+//             if (err) {
+//                 log.error(err, log.getFileNameAndLineNum(__filename));
+//
+//                 socket.emit('authorized', {
+//                     code: constant.returnCode.ERROR,
+//                     msg: err
+//                 });
+//             }else{
+//                 if (code === constant.returnCode.TOKEN_INVALID) {
+//                     log.debug('token invalid');
+//                     socket.emit('authorized', {
+//                         code: constant.returnCode.TOKEN_INVALID,
+//                         msg: 'token invalid'
+//                     });
+//                 }
+//                 if (code === constant.returnCode.TOKEN_VALID) {
+//                     log.debug('token valid');
+//                     socket.emit('authorized', {
+//                         code: constant.returnCode.TOKEN_VALID,
+//                         msg: 'token valid'
+//                     });
+//
+//                     //定义各类事件响应
+//                     ioHandle(socket);
+//                 }
+//             }
+//         });
+//     });
+// }
+
+
+// function ioHandle(socket){
+//     socket.on('data', function(data){
+//         console.log('data event');
+//         socket.emit('data', data);
+//     });
+// }
